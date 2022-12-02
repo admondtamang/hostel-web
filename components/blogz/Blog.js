09e8bbd7b914607d08671892dc9d5d409a255601
admond <admondtamang@gmail.com>
2022-12-02 10:04:42 +0545

    fix: blogs

---
+++ components/blogz/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div className='SectionSliderNewCategories px-12 py-8 bg-[#F3F4F6] rounded-[45px] mx-4'>
+         <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Blog
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,15 +1,16 @@
+import React from 'react'
+import Blog from '../../components/blogz/blog'
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import BlogHome from "../../components/blog/BlogHome";
 
-const blog = () => {
+const index = () => {
   return (
     <div>
-        <Header />
-        <BlogHome />
+      <Header />
+        <Blog />
         <Footer />
     </div>
   )
 }
 
-export default blog
+export default index
