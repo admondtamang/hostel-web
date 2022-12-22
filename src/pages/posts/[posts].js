1c71af9090dffe1e1749c326cddcaec1908157b7
admond <admondtamang@gmail.com>
2022-12-23 01:09:03 +0545

    fix: build issue

--- src/pages/posts/[posts].js
+++ src/pages/posts/[posts].js
@@ -1,47 +1,52 @@
-import Image from "next/image";
-import Head from "next/head";
-import { MDXRemote } from "next-mdx-remote";
-import { serialize } from "next-mdx-remote/serialize";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeHighlight from "rehype-highlight";
-import { getPostFromSlug, getSlugs } from "@/src/api";
-import "highlight.js/styles/atom-one-dark.css";
+// import Image from "next/image";
+// import Head from "next/head";
+// import { MDXRemote } from "next-mdx-remote";
+// import { serialize } from "next-mdx-remote/serialize";
+// import rehypeSlug from "rehype-slug";
+// import rehypeAutolinkHeadings from "rehype-autolink-headings";
+// import rehypeHighlight from "rehype-highlight";
+// import { getPostFromSlug, getSlugs } from "@/src/api";
+// import "highlight.js/styles/atom-one-dark.css";
 
-export default function PostPage({ post }) {
-  return (
-    <>
-      <Head>
-        <title>{post.meta.title}</title>
-      </Head>
-      <h1>{post.meta.title}</h1>
-      <MDXRemote {...post.source} components={{ Image }} />
-    </>
-  );
+// export default function PostPage({ post }) {
+//   return (
+//     <>
+//       <Head>
+//         <title>{post.meta.title}</title>
+//       </Head>
+//       <h1>{post.meta.title}</h1>
+//       <MDXRemote {...post.source} components={{ Image }} />
+//     </>
+//   );
+// }
+
+// export const getStaticProps = async ({ params }) => {
+//   const { slug } = params;
+//   const { content, meta } = getPostFromSlug(slug);
+//   const mdxSource = await serialize(content, {
+//     mdxOptions: {
+//       rehypePlugins: [
+//         rehypeSlug,
+//         [rehypeAutolinkHeadings, { behavior: "wrap" }],
+//         rehypeHighlight,
+//       ],
+//     },
+//   });
+
+//   return { props: { post: { source: mdxSource, meta } } };
+// };
+
+// export const getStaticPaths = async () => {
+//   const paths = getSlugs().map((slug) => ({ params: { slug } }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+// Footer;
+import React from "react";
+
+export default function Posts() {
+  return <div>Posts</div>;
 }
-
-export const getStaticProps = async ({ params }) => {
-  const { slug } = params;
-  const { content, meta } = getPostFromSlug(slug);
-  const mdxSource = await serialize(content, {
-    mdxOptions: {
-      rehypePlugins: [
-        rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: "wrap" }],
-        rehypeHighlight,
-      ],
-    },
-  });
-
-  return { props: { post: { source: mdxSource, meta } } };
-};
-
-export const getStaticPaths = async () => {
-  const paths = getSlugs().map((slug) => ({ params: { slug } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-Footer;
